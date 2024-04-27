c1547a748a7d0e9b2a1f66836fdfcd751eba1f6f
jhw1016-github <janghw54@gmail,com>
2024-04-27 19:40:35 +0900

    second commit

---
+++ src/main/java/com/slowletter/SlowletterApplication.java
@@ -0,0 +1,13 @@
+package com.slowletter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlowletterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlowletterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/slowletter/SlowletterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slowletter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlowletterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
